412531e3661b434d589191d91b166afb6456534b
Sirraide <aeternalmail@gmail.com>
2025-09-22 02:06:45 +0200

    Member profiles

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,18 +1,41 @@
-"use server";
 
 import {Stripe} from '@/app/components';
 import { sql } from 'bun'
 import type {MemberProfile} from '@/app/api';
 
+function Member({
+    member
+}: {
+    member: MemberProfile
+}) {
+    return (
+        <div className='flex gap-2 text-2xl'>
+            <div>
+                <img
+                    src={member.avatar_url}
+                    className='w-8 rounded-full select-none'
+                    alt={member.display_name}
+                />
+            </div>
+            <div className='leading-8'>
+                <span className={member.active ? "" : "line-through text-gray-500"}>
+                    {member.display_name}
+                </span>
+            </div>
+            {member.administrator ? <span className='select-none'>🛡️</span> : null}
+        </div>
+    )
+}
+
 export default async function Page() {
+    'use server';
     const members = await sql`SELECT * FROM members ORDER BY display_name` as MemberProfile[];
+
     return (
         <>
             <Stripe>Members</Stripe>
-            <div>
-                {members.map((m) => <div key={m.discord_id}>
-                    {m.display_name}
-                </div>)}
+            <div className='flex flex-col gap-2'>
+                {members.map((m) => <Member key={m.discord_id} member={m}/>)}
             </div>
         </>
     )
