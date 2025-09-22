46810b989ccf0be5a0b3eed3ce24783ae143e1ef
Sirraide <aeternalmail@gmail.com>
2025-09-22 02:48:13 +0200

    Move links into sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type {Metadata} from 'next';
 import {Libertinus_Sans} from 'next/font/google';
 import './globals.css';
+import Link from 'next/link';
+import {ReactNode} from 'react';
 
 const font = Libertinus_Sans({
     variable: '--font-libertinus-sans',
@@ -13,6 +15,38 @@ export const metadata: Metadata = {
     description: 'Website for managing ongoing affairs of the UŊ',
 };
 
+function Sidebar() {
+    function Section({ title, children }: { title: string, children: ReactNode }) {
+        return (
+            <div>
+                <h3 className='mb-1'>{title}</h3>
+                <div className='flex flex-col'>
+                    {children}
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className='fixed left-0 top-0 h-full w-(--sidebar-width) pl-4 border-r border-r-gray-500 gap-4 flex flex-col'>
+            <Section title='Meetings'>
+                <Link href='/'>Current Meeting</Link>
+                <Link href='/meetings'>Past Meetings</Link>
+            </Section>
+            <Section title='Actions'>
+                <Link href='/motion'>Create a Motion</Link>
+                <Link href='/admission'>Create a Ŋation</Link>
+            </Section>
+            <Section title='Lists'>
+                <Link href='/members'>Members</Link>
+                <Link href='/nations'>Ŋations</Link>
+                <Link href='/motions'>Motions</Link>
+                <Link href='/admissions'>Admissions</Link>
+            </Section>
+        </div>
+    )
+}
+
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -21,7 +55,8 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <body className={`${font.className} antialiased`}>
-                <main className='px-20'>
+                <Sidebar />
+                <main className='px-20 ml-(--sidebar-width)'>
                     {children}
                 </main>
             </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,9 @@
-import {ReactNode} from 'react';
-import Link from 'next/link';
 import {Stripe} from '@/app/components';
 
-export default function MainPage() {
-    function Column({ title, children }: { title: string, children: ReactNode }) {
-        return (
-            <div>
-                <h3 className='mb-2'>{title}</h3>
-                <div className='flex flex-col'>
-                    {children}
-                </div>
-            </div>
-        )
-    }
-
+export default function CurrentMeeting() {
     return (
         <>
-            <Stripe>Main Page</Stripe>
-            <section className='flex flex-row gap-10 justify-center'>
-                <Column title='Meetings'>
-                    <Link href='/meeting'>Next Meeting</Link>
-                    <Link href='/meetings'>Past Meetings</Link>
-                </Column>
-                <Column title='Actions'>
-                    <Link href='/motion'>Create a Motion</Link>
-                    <Link href='/admission'>Create a Ŋation</Link>
-                </Column>
-                <Column title='Lists'>
-                    <Link href='/members'>Members</Link>
-                    <Link href='/nations'>Ŋations</Link>
-                    <Link href='/motions'>Motions</Link>
-                    <Link href='/admissions'>Admissions</Link>
-                </Column>
-            </section>
+            <Stripe>Current Meeting</Stripe>
         </>
     );
 }
